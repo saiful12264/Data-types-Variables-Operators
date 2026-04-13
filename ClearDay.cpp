cdbcfb34f981e7fabf96c1ef8cd8eee3d157b1fe
saiful12264 <saifulislam12264@gmail.com>
2026-04-13 07:42:58 +0600

    Clear Day

---
+++ ClearDay.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+      int x,y;
+      cin>>x>>y;
+
+      cout<<(7-x-y)<<endl;
+
+     
+    return 0;
+}
